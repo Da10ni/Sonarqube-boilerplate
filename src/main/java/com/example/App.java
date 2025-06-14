241096b9b44c676205831911624de3ae68564353
Moiz <moiz@gmail.com>
2025-06-14 18:47:21 +0500

    updates

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,5 +3,6 @@ package com.example;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello from SonarQube sample project!");
+        // This is a sample Java application to demonstrate SonarQube analysis
     }
 }
